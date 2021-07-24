077d556b9cdab814e69acc13620d2dcfd0e4c695
DitaGuerrero <dita66uerrero@gmail.com>
2021-07-24 15:12:40 -0700

    createWeatherTemplate function implemented * Added styles to answer section

--- js/apiRequests.js
+++ js/apiRequests.js
@@ -1,10 +1,10 @@
-const apiKey = "xcgqyKOELLfF34Bu5gCCAsPomLCBiLnR";
+// const apiKey = "xcgqyKOELLfF34Bu5gCCAsPomLCBiLnR";
+const apiKey = "davq7QGzhSJO9muojHI3lDwMODeqEAFA"; //Edu apikey
 const cityURI =
     "http://dataservice.accuweather.com/locations/v1/cities/search?";
 const weatherURI =
     "http://dataservice.accuweather.com/currentconditions/v1/";
 
-console.log(process.env.TEST);
 //A different version using async - await
 // const getKeyFromFirstCity = (city) =>
 //     new Promise(async (resolve, reject) => {
@@ -21,15 +21,18 @@ console.log(process.env.TEST);
 //         }
 //     });
 
-const getKeyFromFirstCity = (city) =>
+const getKeyFromFirstCity = (cityString) =>
     new Promise((resolve, reject) => {
         const url =
             cityURI +
-            `apikey=${apiKey}&q=${city.trim().replace(" ", "%20")}`;
+            `apikey=${apiKey}&q=${cityString
+                .trim()
+                .replace(" ", "%20")}`;
         fetch(url)
             .then((response) => response.json())
-            .then((data) => {
-                resolve(data[0].Key);
+            .then((cits) => {
+                console.log(cits[0]);
+                resolve(cits[0]);
             })
             .catch((error) => {
                 reject(error);
@@ -41,9 +44,9 @@ const getDataByCityId = (cityId) =>
         const url = weatherURI + `${cityId}?apikey=${apiKey}`;
         fetch(url)
             .then((response) => response.json())
-            .then((data) => {
-                console.log(data[0]);
-                resolve(data[0]);
+            .then((weatherDetails) => {
+                console.log(weatherDetails[0]);
+                resolve(weatherDetails[0]);
             })
             .catch((error) => {
                 reject(error);
--- js/appScript.js
+++ js/appScript.js
@@ -1,10 +1,56 @@
 const form = document.querySelector("form");
-form.addEventListener("submit", (e) => {
+const answerSection = document.querySelector("div.answerSection");
+
+//Create weather cards with current conditions
+const createWeatherTemplate = (city, weather, answerSection) => {
+    answerSection.innerHTML = "";
+
+    //Image background
+    let imgBackgroundUrl = weather.IsDayTime
+        ? "./img/day.svg"
+        : "./img/night.svg";
+    const weatherImgBackground = document.createElement("img");
+    weatherImgBackground.setAttribute("src", imgBackgroundUrl);
+    weatherImgBackground.classList.add("weatherImgBackground");
+
+    //Icon selection
+    const weatherImg = document.createElement("img");
+    let iconUrl = `./img/icons/${weather.WeatherIcon}.svg`;
+    weatherImg.setAttribute("src", iconUrl);
+    weatherImg.classList.add("weatherImg");
+
+    //City Name
+    const cityName = document.createElement("h1");
+    cityName.innerHTML = `${city.EnglishName} ${city.Country.EnglishName}`;
+
+    //Weather Text
+    const weatherText = document.createElement("h2");
+    weatherText.innerHTML = `${weather.WeatherText}`;
+
+    //Temperature
+    const temperature = document.createElement("h2");
+    temperature.innerHTML = `${weather.Temperature.Metric.Value} &deg C`;
+
+    answerSection.appendChild(weatherImgBackground);
+    answerSection.appendChild(weatherImg);
+    answerSection.appendChild(cityName);
+    answerSection.appendChild(weatherText);
+    answerSection.appendChild(temperature);
+};
+
+form.addEventListener("submit", async (e) => {
     e.preventDefault();
-    getKeyFromFirstCity(e.target[0].value)
-        .then((cityId) => {
-            console.log(cityId);
-            getDataByCityId(cityId);
-        })
-        .catch(onerror);
+    let firstCity;
+    let weatherDetails;
+    try {
+        firstCity = await getKeyFromFirstCity(e.target[0].value);
+        weatherDetails = await getDataByCityId(firstCity.Key);
+        createWeatherTemplate(
+            firstCity,
+            weatherDetails,
+            answerSection
+        );
+    } catch (e) {
+        console.log(e);
+    }
 });
